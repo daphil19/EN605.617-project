42e96bc909060cd55a7c68043292aeb6d1548528
David Phillips <daphillips19@gmail.com>
2022-03-31 01:51:45 +0000

    get started with some fftw code

--- fftw_driver.c
+++ fftw_driver.c
@@ -1,9 +1,24 @@
 #include <fftw3.h>
 
-int main() {
+int main()
+{
     // these are double-precision; see https://www.fftw.org/fftw3_doc/Complex-numbers.html
     fftw_complex *in, *out;
     fftw_plan p;
+
+    size_t N = 8192;
+
+    // using fftw_malloc is the reccomended approach for allocating data
+    in = (fftw_complex *)fftw_malloc(sizeof(fftw_complex) * N);
+    out = (fftw_complex *)fftw_malloc(sizeof(fftw_complex) * N);
+
+    // TODO explain!
+    p = fftw_plan_dft_1d(N, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+
+    fftw_destroy_plan(p);
+
+    fftw_free(in);
+    fftw_free(out);
 
     return 0;
 }
